a4b277041c833d992f071510bd997c0916ab1b48
Jaime Ashander <jashander@ucdavis.edu>
2016-03-12 18:28:34 -0800

    Module for datastreams only, with three functions
    available: stream GUIDs for datastreams to stdout
    info(guid, callback): metadata for a stream
    data(guid, callback): stream the data
    
    calling without callbacks streams JSON to stdout

--- index.js
+++ index.js
@@ -1,128 +1,152 @@
-// http://data.saccounty.net/developers/
-//A typical request will look like this:
-//http://saccounty.cloudapi.junar.com/api/v2/datastreams/SACRA-COUNT-CALIF-QUICK-FACTS/data.json/?auth_key=YOUR_API_KEY
-//A few things to note in the request above: /api/v2/datastreams/ This is an
-//invoke request, that will return a json response. The entire resources
-//descriptions are available in the API resource documentation.
-//SACRA-COUNT-CALIF-QUICK-FACTS This is the GUID of the data stream you are
-//accessing. You will always find this GUID in the Data Stream Details page,
-//making a data stream search through the API, or in many other places.
-//auth_key=YOUR_API_KEY This is the key from step 1. You"ll need it for every
-//request.
+// scram: sacramento county retriever and munger
+// Wrap Sac Open Data API
 //
-// Your response will be returned as JSON.
-// You can also obtain the response in XML, CSV and HTML..
+// Exports three fuctions:
+//
+// - available() :
+//     stream the guids available in datastreams to process.stdout
+// - info(guid, infoProcessor):
+//     print info about a guid (or process with a custom callback function
+//     infoProcessor)
+//
+// - data(guid, limit =50, streamProcessor):
+//     stream data from guid to process.stdout (or process with a custom
+//     callback function streamProcessor)
+//
+//Details from http://data.saccounty.net/developers/
+//
+// A typical request will look like this:
+//
+// http://saccounty.cloudapi.junar.com/api/v2/datastreams/SACRA-COUNT-CALIF-QUICK-FACTS/data.json/?auth_key=YOUR_API_KEY
+//
+// A few things to note in the request above: /api/v2/datastreams/ This is an
+// invoke request, that will return a json response. The entire resources
+// descriptions are available in the API resource documentation.
+// SACRA-COUNT-CALIF-QUICK-FACTS This is the GUID of the data stream you are
+// accessing. You will always find this GUID in the Data Stream Details page,
+// making a data stream search through the API, or in many other places.
+// auth_key=YOUR_API_KEY This is the key from step 1. You"ll need it for every
+// request.
+//
+// Your response will be returned as JSON.  You can also obtain the response in
+// XML, CSV and HTML..
 //
 // http://api-en.readthedocs.org/en/latest/
-//fs.readFileSync("sac_api.key", "utf8").trim();
 
 var dotenv = require("dotenv").config();
-var fs = require("fs");
 var request = require("request");
-var prompt = require("prompt");
 var JSONStream = require("JSONStream");
 var throughObj = require("through2").obj;
 var concat = require("concat-stream");
 
 var api_key = process.env.SAC_API_KEY;
 var api = "http://saccounty.cloudapi.junar.com/api/v2";
+var default_limit = 50;
 
+exports = module.exports;
 
+exports.available =  function() {
+  limit = false;
+  streamSomething("datastreams/", limit, listGUIDs);
 
-module.exports =  function(category, jsonQuery) {
-  if (typeof jsonQuery === "undefined") {
-    jsonQuery =  "*";
+  function listGUIDs(theStream) {
+    theStream.on("error", handleError);
+    theStream.on("response", function() {
+      theStream
+      .pipe(JSONStream.parse("*.guid"))
+      .pipe(throughObj(function(line, _, next) {
+        this.push(line + "\n");
+        next();
+      }))
+      .pipe(process.stdout);
+
+    });
   }
-  if (typeof category === "undefined") {
-    category = "";
-  }
-  hitSac(category, "yo", jsonQuery);
+
 };
 
+exports.info = function(guid, infoProcessor) {
+  if(typeof infoProcessor === "undefined"){
+    infoProcessor = function(info) { console.log(info);};
+  }
+  limit = false;
+  return streamSomething("datastreams/" + guid, limit, getInfo);
+
+  function getInfo(theStream) {
+
+    var intoJSON= concat(function(buffer) {
+      infoProcessor(JSON.parse(buffer));
+    });
+
+    theStream
+    .on("error", handleError)
+    .pipe(intoJSON);
+  }
+};
+
+exports.data = function(guid, limit, streamProcessor) {
+  if (typeof streamProcessor === "undefined") {
+
+    //probably limit just undefined
+    if (typeof limit === "number") {
+      streamProcessor = defaultStreamProcessor;
+    }
+    if (typeof limit === "undefined") {
+      streamProcessor = defaultStreamProcessor;
+      limit = default_limit;
+    }
+  }
+  console.log("limit: " + limit);
+  return streamSomething("datastreams/" + guid + "/data.json/",
+                         limit, streamProcessor);
+};
+
+function defaultStreamProcessor(theStream) {
+        theStream.pipe(process.stdout);
+        //.pipe(JSONStream.parse("*"))
+      }
+
+function streamSomething(endpoint, limit, callback) {
+  if (typeof limit === "undefined") {
+    limit = default_limit;
+  }
+  var theStream = sacApiRequest(endpoint, limit); //fakeSac(endpoint);
+
+  return callback(theStream);
+}
 
 //with request
-function getSac(endpoint) {
+function sacApiRequest(endpoint, limit) {
   var opts = {
     baseUrl: api,
     method: "GET",
-    qs: {auth_key: api_key}
+    qs: {"auth_key": api_key}
   };
-//  console.log(opts);
+  if(limit) {
+    opts.qs.limit = limit;
+  }
+  //  console.log(opts);
   var r = request(endpoint, opts);
   return r;
 }
 
-function hitSac(endpoint, prompt_choice, jsonQuery) {
-  var eventToPipe = "response"; //"open" // for fake sack
-  var menu;
-
-  var input = getSac(endpoint); //fakeSac(endpoint);
-
-  input.on("error", handleError);
-  input.on(eventToPipe, goIn);
-
-  function echoResult(err, result){
-    console.log(result);
-  }
-  function topLevel(){
-    var listMenu = concat(listKeys);
-    input.pipe(listMenu).on("finish", function(err, echoResult){
-      console.log(prompt_choice);
-      prompt.get("choice", function(err, result) {
-        console.log("You chose \'" + result.choice + "\'");
-        //        echoResult(null, result.choice);
-      });} );
-  }
-
-  function goIn(){
-    //    var listMenu = concat(listKeys);
-    //  input.on("error",  function(err) {console.log(err)})
-    input
-    .pipe(JSONStream.parse(jsonQuery))
-    .pipe(throughObj(function(line, _, next) {
-      this.push(JSON.stringify(line) + "\n");
-      next();
-    }))
-    .pipe(process.stdout);
-    //	    .on("data", function (chunk) {
-    //		    console.log(JSON.stringify(chunk));
-    //	    });
-    /////////////////////////cruft in this function
-    //
-    //.pipe(fs.createWriteStream(category + ".json"))
-    //    .pipe(JSONStream.stringify())
-
-    //	    .pipe(listMenu)
-    //	    .on("finish", function(err, echoResult){
-    //      console.log(prompt_choice);
-    //      prompt.get("choice", function(err, result) {
-    //        console.log("You chose \'" + result.choice + "\'");
-    //        echoResult(null, result.choice);
-    //      });} );
-  }
-
-  //
-  //.pipe(fs.createWriteStream("top_level.json"))
-  //  .on("end", function() {
-  //      //done
-  //   })
-  //console.log(r)
-}
-
-function fakeSac(endpoint){
-  var fakeSac = fs.createReadStream(category + ".json");
-  return fakeSac;
-}
-
-function listKeys(menu) {
-  menu = JSON.parse(menu);
-  var ctr = 0;
-  for(var key in menu){
-    console.log(++ctr +") "+ key);
-  }
-}
 function handleError(err) {
   // handle your error appropriately here, e.g.:
   console.error(err);// print the error to STDERR
   process.exit(1);// exit program with non-zero exit cod
 }
+
+  //var eventToPipe = "response"; //"open" // if mock
+//.pipe(JSONStream.parse(jsonQuery));
+    //on(eventToPipe, );
+
+//
+//  function goIn(){
+//    input
+//    .pipe(JSONStream.parse(jsonQuery))
+//    .pipe(throughObj(function(line, _, next) {
+//      this.push(JSON.stringify(line) + "\n");
+//      next();
+//    }))
+//    .pipe(process.stdout);
+//  }
--- scram-cli.js
+++ scram-cli.js
@@ -1,16 +1,33 @@
 var scram = require("scram");
 
-endpoint = process.argv[2];
-jsonpath = process.argv[3];
+type= process.argv[2];
+guid = process.argv[3];
+limit = process.argv[4];
 
-if(typeof endpoint === "undefined") {
-console.log("Usage: node scram-cli (ENDPOINT='') (JSONPATH='*')");
-console.log("");
-console.log("Calling without arguments will return the possible endpoints");
-console.log("(last part of the URL).");
-console.log("");
-console.log("The JSONPATH, if provided, is used to filter the output");
-console.log("");
+function printHelp() {
+  console.log("Explore Sac County Open datastreams");
+  console.log("");
+  console.log("Usage: node scram-cli (info|data) [GUID] [limit=50]");
+  console.log("");
+  console.log("Calling without arguments will return the possible GUIDs");
+  console.log("");
 }
-console.log("Results:");
-scram(endpoint, jsonpath);
+
+if (typeof type === "undefined" && typeof guid === "undefined") {
+  scram.available();
+}
+
+if (type === "info" || type === "data" || type === "-h" || type === "--help"){
+  if(!/h/.test(type) && typeof guid === "undefined") {
+    console.log("Enter a guid!");
+  } else {
+    if (type === "info") {
+      scram.info(guid);
+    }
+    if (type === "data") {
+      scram.data(guid, limit);
+    }
+  }
+  printHelp();
+
+}
